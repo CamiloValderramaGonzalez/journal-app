92268eb7e45b37e9953def8d1294c8c66ef5a159
Camilo Valderrama <camilovalderrama@gmail.com>
2022-04-12 18:30:35 -0300

    configuracion de thunk

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,22 +1,44 @@
+import { useDispatch } from "react-redux"
 import { Link } from "react-router-dom"
+import { login } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm"
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const [formValues, handleInputChange, reset] = useForm({
+        email: 'a@a.com',
+        password: '123456'
+    })
+
+    const { email, password } = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(login(123456, 'camilo'));
+    }
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
-            <form>
+            <form onSubmit={handleLogin}>
                 <input
-                    className="auth__input"
-                    type="text"
-                    placeholder="Email"
-                    name="email"
                     autoComplete="off"
+                    className="auth__input"
+                    name="email"
+                    placeholder="Email"
+                    type="text"
+                    value={email}
+                    onChange={handleInputChange}
                 />
                 <input
                     className="auth__input"
-                    type="password"
-                    placeholder="Password"
                     name="password"
+                    placeholder="Password"
+                    type="password"
+                    value={password}
+                    onChange={handleInputChange}
                 />
                 <button
                     className="btn btn-primary btn-block"
